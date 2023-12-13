fa6338259d72e0bba208c5076e135685b6fcc332
SKodehode <stiankodehode@gmail.com>
2023-12-13 11:23:24 +0100

    Updated data.js with correct capitalization. Created a function in the ProjectCards.jsx document that will display the information in the object array in data.js in a card-like way. Updated the CSS for the project-cards and project's page for the cards to be displayed better.

--- src/components/projectcards/ProjectCards.jsx
+++ src/components/projectcards/ProjectCards.jsx
@@ -1,7 +1,27 @@
-export default function ProjectCards() {
+import "./ProjectCards.css"
+import GithubIcon from "../../assets/icons/github_icon"
+import HomeIcon from "../../assets/icons/home_icon"
+import Buttons from "../buttons/Buttons"
+
+export default function ProjectCards({ posts }) {
     return(
         <>
-            
+            {posts.map((post) => (
+                <div className="card-wrapper" key={post.id}>
+                    <div className="card-info">
+                        <h2 className="card-title">{post.title}</h2>
+                        <p className="card-description">{post.description}</p>
+                        <div>
+                            <Buttons to={post.githublink} icon={<GithubIcon/>} text="GitHub" external={true} />
+                            <Buttons to={post.livepage} icon={<HomeIcon/>} text="Visit" external={true} />
+                        </div>
+                    </div>
+                    <div className="Project-Image">
+                        <img className="card-image" src={post.image} alt={post.title} />
+                    </div>
+                </div>
+
+            ))}
         </>
     )
 }
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,11 +1,16 @@
 import "./projects.css"
 import LayoutBase from "../../components/layout/LayoutBase"
+import ProjectCards from "../../components/projectcards/ProjectCards"
+import { data } from "../../components/data/data"
 
 export default function ProjectsPage() {
     return(
         <LayoutBase>
             <div id="projectspage">
-               <p>I am the project's page.</p> 
+               <p>I am the project's page.</p>
+               <div id="projects-wrapper">
+                    <ProjectCards posts={data} />
+               </div>
             </div>
         </LayoutBase>
     )
