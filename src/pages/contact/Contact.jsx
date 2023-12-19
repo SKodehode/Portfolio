45ece3992006062e46c49422545f0491344cb083
SKodehode <stiankodehode@gmail.com>
2023-12-19 09:33:25 +0100

    Made a change to Projects.js' capitilization due to build error.

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -6,7 +6,13 @@ export default function ContactPage() {
     return(
         <LayoutBase>
             <div id="contactpage">
-                <p>I am the contact me page.</p>
+                <h3>Contact me</h3>
+                <form action="https://formsubmit.co/stkvinnesland@gmail.com" method="POST">
+                    <input type="text" name="name" min="2" required /> 
+                    <input type="email" name="email" min="2" required />
+                    <input type="text" name="message" min="2" required />
+                    <button type="submit">Send</button>
+                </form>
             </div>
         </LayoutBase>
     )
