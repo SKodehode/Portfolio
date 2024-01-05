82a83b728d6743431dbd46ac089e7a90969aaa6b
SKodehode <stiankodehode@gmail.com>
2024-01-05 17:30:35 +0100

    Split about me paragraphs into different p tags to space them out more.

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -19,7 +19,10 @@ export default function AboutPage() {
                 <div id="about-info-container">
                     <div id="about-wrapper" className="about-page-wrappers">
                         <h2 id="about-title" className="about-page-titles">About me</h2>
-                        <p id="about-text" className="about-page-texts">My name is Stian Kvinnesland and I recently completed a course where I had the pleasure of learning HTML, CSS, JavaScript, React and SQL over the last 6 months. Now I'm looking for a potential workplace where I can utilise my newly learned skills and grow as a developer.<br/>While I spent the last 6 months learning coding I do have a journeyman certificate as a ICT technician. I'm very fond of technology and working with machines and computer systems and it is a hobby of mine that I have had for several years. And learning how to code was the natural next step for me to increase my knowledge in the field of technology.<br/>I'm an avid video game enjoyer as well and in my spare time I enjoy playing games where I can spend time with friends. I'm particularly fond of the genre massive multiplayer games where you work together as a team to overcome challenges.<br/>I look forward to hearing from you!</p>
+                        <p id="about-text" className="about-page-texts">My name is Stian Kvinnesland and I recently completed a course where I had the pleasure of learning HTML, CSS, JavaScript, React and SQL over the last 6 months. Now I'm looking for a potential workplace where I can utilise my newly learned skills and grow as a developer.</p>
+                        <p id="about-text" className="about-page-texts">While I spent the last 6 months learning coding I do have a journeyman certificate as a ICT technician. I'm very fond of technology and working with machines and computer systems and it is a hobby of mine that I have had for several years. And learning how to code was the natural next step for me to increase my knowledge in the field of technology.</p>
+                        <p id="about-text" className="about-page-texts">I'm an avid video game enjoyer as well and in my spare time I enjoy playing games where I can spend time with friends. I'm particularly fond of the genre massive multiplayer games where you work together as a team to overcome challenges.</p>
+                        <p id="about-text" className="about-page-texts">I look forward to hearing from you!</p>
                     </div>
                     <div id="knowledge-wrapper" className="about-page-wrappers">
                         <h2 id="knowledge-title" className="about-page-titles">My Knowledge</h2>
@@ -30,7 +33,8 @@ export default function AboutPage() {
                             <Buttons to="https://react.dev/learn" icon={<ReactIcon/>} text="React" external={true} />
                             <Buttons to="https://www.figma.com/" icon={<FigmaIcon/>} text="Figma" external={true} />
                         </ul>
-                        <p id="knowledge-text" className="about-page-texts">I began developing my skills as a front-end developer at a course supplied by a company named Jobloop in Haugesund, Norway. In the 6-months I've spent with them I got to explore and learn HTML, CSS which was the stepping stones to what I learned later on in the course, and what I found myself enjoying the most, Javascript and React.<br/>  Between learning these tools I also got to learn and work with Figma. Learning it by recreating sites at first and by the end I designed my own portfolio page before writing it. </p>
+                        <p id="knowledge-text" className="about-page-texts">I began developing my skills as a front-end developer at a course supplied by a company named Jobloop in Haugesund, Norway. In the 6-months I've spent with them I got to explore and learn HTML, CSS which was the stepping stones to what I learned later on in the course, and what I found myself enjoying the most, Javascript and React.</p>
+                        <p id="knowledge-text" className="about-page-texts">Between learning these tools I also got to learn and work with Figma. Learning it by recreating sites at first and by the end I designed my own portfolio page before writing it. </p>
                     </div>
                 </div>
             </div>
