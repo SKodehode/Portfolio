d59c4409d3a5599a902f4b748da80aad61a4b061
SKodehode <stiankodehode@gmail.com>
2024-01-15 11:01:48 +0100

    Updated the contact form style and html.

--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -6,19 +6,29 @@ export default function ContactPage() {
     return(
         <LayoutBase>
             <div id="contactpage">
-                <div id="contact-form">
-                    <h3>Contact me</h3>
                     <form id="contact-form" action="https://formsubmit.co/stkvinnesland@gmail.com" method="POST">
-                        <h4>Name:</h4>
-                        <input className="contact-input-field" id="contact-name" type="text" name="name" min="2" required /> 
-                        <h4>Email:</h4>
-                        <input className="contact-input-field" id="contact-email" type="email" name="email" min="2" required />
-                        <h4>Message:</h4>
-                        <input className="contact-input-field" id="contact-text" type="text" name="message" min="2" required />
-                        <button id="contact-button" type="submit">Send</button>
+                        
+                        <h3 id="Contact-page-title">Contact me</h3>
+                        <div className="form-group-name-email">
+                            <div className="form-group" id="form-group-name">
+                                <label for="contact-name" className="form-title">Name:</label>
+                                <input className="contact-input-field" id="contact-name" type="text" name="name" min="2" required maxLength="50"/>
+                            </div>
+                            <div className="form-group" id="form-group-email">
+                                <label for="contact-email" className="form-title">Email:</label>
+                                <input className="contact-input-field" id="contact-email" type="email" name="email" min="2" required maxLength="100"/>
+                            </div>
+                        </div>
+                        <div className="form-group" id="form-group-message">
+                            <label for="contact-text" className="form-title">Message:</label>
+                            {/* <input className="contact-input-field" id="contact-text" type="text" name="message" min="2" required /> */}
+                            <textarea className="contact-input-field" id="contact-text" type="text" rows="15" cols="50" required="required" min="2" maxLength="2000"></textarea>
+                        </div>
+                        <div className="form-group" id="form-group-buttons">
+                            <button id="contact-button" type="submit">Submit</button>
+                        </div>
                         <input type="hidden" name="_next" value="https://portfolio-5np.pages.dev/thank-you"></input>
                     </form>
-                </div>
             </div>
         </LayoutBase>
     )
