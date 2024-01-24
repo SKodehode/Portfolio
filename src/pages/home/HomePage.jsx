be755582b2f62d3e33148886986a16676e58e1dd
SKodehode <stiankodehode@gmail.com>
2024-01-24 11:40:06 +0100

    Updated the resume link on the home page.

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -14,7 +14,7 @@ export default function HomePage() {
                 <div id="hero-container">
                     <p id="hero-title">Hi, my name is <br/><span id="hero-name">Stian Kvinnesland</span><br/>and I am a Front-End developer</p>
                     <ul className="hero-buttons">
-                        <Buttons to="https://drive.google.com/file/d/1C8f-1BFVbBS2O2S6ifxNwQ7vEhkOEcyS/view?usp=sharing" icon={<ResumeIcon/>} text="Resume" external={true} />
+                        <Buttons to="https://drive.google.com/file/d/1U9xncU0-Zp5TTSQVVvAf2E5LnVThkkIq/view?usp=sharing" icon={<ResumeIcon/>} text="Resume" external={true} />
                         <Buttons to="https://github.com/SKodehode" icon={<GithubIcon/>} text="GitHub" external={true} />
                         <Buttons to="https://linkedin.com/in/stian-kvinnesland-079289113" icon={<LinkedinIcon/>} text="LinkedIn" external={true} />
                     </ul>
